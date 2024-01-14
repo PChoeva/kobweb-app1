56d5ef357ad32e8139ccbf27946a31f106eb6613
Petya Choeva <petya.choeva@gmail.com>
2024-01-14 19:32:20 +0000

    Added input and dynamic text display field

--- site/src/jsMain/kotlin/com/pcapps/app1/pages/Index.kt
+++ site/src/jsMain/kotlin/com/pcapps/app1/pages/Index.kt
@@ -1,18 +1,58 @@
 package com.pcapps.app1.pages
 
 import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.fontSize
+import com.varabyte.kobweb.compose.ui.modifiers.margin
+import com.varabyte.kobweb.compose.ui.modifiers.padding
+import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.silk.components.icons.fa.FaFaceSmile
+import com.varabyte.kobweb.silk.components.icons.fa.IconSize
+import com.varabyte.kobweb.silk.components.icons.fa.IconStyle
+import org.jetbrains.compose.web.attributes.InputType
+import org.jetbrains.compose.web.attributes.placeholder
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.dom.Input
+import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
 
 @Page
 @Composable
 fun HomePage() {
-    // TODO: Replace the following with your own content
-    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("THIS PAGE INTENTIONALLY LEFT BLANK")
+    var name by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            FaFaceSmile(
+                modifier = Modifier.margin(right = 10.px),
+                style = IconStyle.FILLED,
+                size = IconSize.XXL
+            )
+            P(attrs = Modifier
+                .fontSize(40.px)
+                .toAttrs()) {
+                Text(value = "Hello, $name")
+            }
+        }
+        Input (
+           type = InputType.Text,
+           attrs = Modifier
+               .padding(topBottom = 10.px, leftRight = 20.px)
+               .fontSize(20.px)
+               .toAttrs {
+                   placeholder("Enter your name")
+                   onInput { name = it.value  }
+               }
+        )
     }
 }
